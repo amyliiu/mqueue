8423722b082e554718098bda0b47ccb42c86050b
Amy Liu <amyziyuliu@gmail.com>
2024-11-19 21:43:27 -0500

    add backend_api_URL

--- client/.next/server/webpack-runtime.js
+++ client/.next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("7bc02e356609df48")
+/******/ 		__webpack_require__.h = () => ("a76dbc1ce5ccc406")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("18f7858aa6d2d895")
+/******/ 		__webpack_require__.h = () => ("d1d166baf774d873")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
